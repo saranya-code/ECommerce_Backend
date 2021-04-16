aeb170a41ea5fd59a52117704817751161e778c1
saranya-code <saranyadayalan28@gmail.com>
2021-04-16 10:46:40 -0500

    Category route GET PUT POST DELETE working fine

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,9 +6,7 @@ const { Category, Product} = require('../../models');
 router.get('/', async (req, res) => {
     try {
         const categoryData = await Category.findAll({
-            include: { 
-                model: Product
-            },
+            include: { model: Product },
         });
         res.status(200).json(categoryData);
     } catch (err){
@@ -16,14 +14,46 @@ router.get('/', async (req, res) => {
     }
 });
 
+//GET Category by Id
+router.get('/:id', async (req, res) => {
+    try {
+        const categoryData = await Category.findByPk(req.params.id, {
+             include:  { model: Product },
+        });
+        res.status(200).json(categoryData);
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
 
-//GET by id
-//CREATE using POST
-//DELETE
+//Create new catogory using POST
+router.post('/', async (req, res) => {
+    try {
+        const categoryData = await Category.create(req.body);
+        
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
+//DELETE category by Id
+router.delete('/:id', async (req, res) => {
+    try {
+        const categoryData = await Category.destroy({
+            where:{
+                id: req.params.id
+            },
+        });
 
+        if (!categoryData) {
+            res.status(404).json({ message: 'No Category found with that id!' });
+            return;
+          }
 
-
-
-
+        res.status(200).json(locationData);
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
